e371d074b174155165d8876268fd71cdd82257b9
Cris <barcelona1252@gmail.com>
2022-03-04 19:09:31 +0000

    Created the POJO of Testimonials

---
+++ data/ClientTestimonials.js
@@ -0,0 +1,22 @@
+export const clientTestimonials = [
+    {
+        "id": 1,
+        "clientName": "Karel Miguet",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    }, 
+    {
+        "id": 2,
+        "clientName": "Daniele Tissiman",
+        "description": "Aliquam interdum semper sem eget aliquet."
+    }, 
+    {
+        "id": 3,
+        "clientName": "Fifi Conaghy",
+        "description": "Proin dictum elit nec neque malesuada, in fringilla turpis tincidunt."
+    }, 
+    {
+        "id": 4,
+        "clientName": "Deirdre Ewers",
+        "description": "Aenean imperdiet enim tempor nulla luctus pharetra."
+    }
+]
